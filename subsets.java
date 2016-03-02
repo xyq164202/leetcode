cb951f06305ccfbd80a5dbded70ada38105ae6a5
xqy <xyq164202@gmail.com>
2016-03-02 11:49:53 -0600

    another way

--- subsets.java
+++ subsets.java
@@ -1,3 +1,26 @@
-public class Solution {
-    
+class Solution {
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
+                // write your code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(nums == null || nums.length == 0){
+            return result;
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        subsetsHelper(result, list, nums, 0);
+        return result;
+    }
+    public void subsetsHelper(ArrayList<ArrayList<Integer>> result, 
+    ArrayList<Integer> list, int[] nums, int pos){
+        result.add(new ArrayList<Integer> (list));
+        for(int i= pos; i < nums.length; i++){
+            list.add(nums[i]);
+            subsetsHelper(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
 }
